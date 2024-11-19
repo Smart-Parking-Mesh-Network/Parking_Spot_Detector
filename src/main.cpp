5af4b0d21b6735d2a9bbb18efd83ba4e3f4d981d
adel4780 <adelk138019@gmail.com>
2024-11-19 12:27:55 +0330

    Distance measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// SRF04-05 Module
+const int trigPin = 9;
+const int echoPin = 8;
+const int spacePin = 12;
+long duration; 
+int distance;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(spacePin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Distance measurement 
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+  Serial.println(distance);
+  delay(1);
 }
